5c2c8cdece41c638e98143b13f2dc44422fb2f77
Ubuntu <ubuntu@ip-172-31-46-82.ap-south-1.compute.internal>
2026-07-01 17:16:35 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-01a00762f46d584a1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "First_Terraform"
+  }
+}
+
